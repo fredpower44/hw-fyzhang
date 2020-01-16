c736fbac693dae1e643104c0a5f83db42e507ab1
Frederick Zhang <fyzhang@usc.edu>
2020-01-15 19:41:55 -0800

    fdlsjfsd

---
+++ hw1/rational/rational.cpp
@@ -0,0 +1,34 @@
+#include <stdexcept>
+#include "rational.h"
+
+Rational::Rational()
+{
+    n = 0;
+    d = 1;
+}
+Rational::Rational(int num, int denom)
+{
+    if (denom == 0) throw std::invalid_argument("Can't have denom = 0");
+    // Continue your implementation below.
+    // You should likely call this->reduce() and this->normalize0()
+    n = num;
+    d = denom;
+}
+
+static int Rational::gcd(int a, int b) {
+
+}
+
+static int Rational::lcm(int a, int b) {
+
+}
+
+void Rational::normalize0() {
+
+}
+
+void Rational::normalize0()
+{
+    if (n == 0 && d != 1) d = 1;
+}
+
